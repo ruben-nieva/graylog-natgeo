4d65798e0cda34cb9e866157163cf8fe20060b19
Ruben Nieva <ruben.nieva@globant.com>
2016-09-25 23:48:16 -0300

    Added scritp to disable hugepages on mongodb modules.

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,18 +1,33 @@
 class graylog_natgeo::mongodb{
 
-class { '::mongodb::globals':
+$disable_hugepages = '/etc/init.d/disable-transparent-hugepages'
+
+class {'::mongodb::globals':
   manage_package_repo => true,
   version => '3.0.4'
-} ->
-class { '::mongodb::server':
-  smallfiles => true,
-  bind_ip    => ['0.0.0.0'],
-  replset    => 'rsmain'
-} ->
-mongodb_replset {'rsmain':
-  members => ['graylog-mongo01:27017', 'graylog-mongo02:27017', 'graylog-mongo03:27017' ],
+}->
+
+class {'::mongodb::server':
+  replset => 'rsmain',
+  replset_config => { 'rsmain' => { ensure  => present, members => ['graylog-mongo01:27017', 'graylog-mongo02:27017', 'graylog-mongo03:27017']  }  },
+  bind_ip => ['0.0.0.0']
 }
 
 #class {'::mongodb::client':}
+
+file { "$disable_hugepages":
+    ensure => present,
+    #source => "files/disable-transparent-hugepages",
+    source => 'puppet:///modules/graylog_natgeo/disable-transparent-hugepages',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+ }
+
+exec {"run_on_boot_up":
+  require => File[$disable_hugepages],
+  command => "update-rc.d disable-transparent-hugepages defaults",
+  path    => '/usr/sbin:/usr/bin/:/bin/:/sbin/',
+}
 
 }
