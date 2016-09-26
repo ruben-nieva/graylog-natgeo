0ba24cfb1968ca6c946eeecab8a9a85422c672d7
Ruben Nieva <ruben.nieva@globant.com>
2016-09-25 22:45:21 -0300

    Modified mongodb module.

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,16 +1,18 @@
 class graylog_natgeo::mongodb{
 
-class {'::mongodb::globals':
+class { '::mongodb::globals':
   manage_package_repo => true,
   version => '3.0.4'
-}->
+} ->
+class { '::mongodb::server':
+  smallfiles => true,
+  bind_ip    => ['0.0.0.0'],
+  replset    => 'rsmain'
+} ->
+mongodb_replset {'rsmain':
+  members => ['graylog-mongo01:27017', 'graylog-mongo02:27017', 'graylog-mongo03:27017' ],
+}
 
-class {'::mongodb::server':
-  replset => 'rsmain',
-  replset_config => { 'rsmain' => { ensure  => present, members => ['graylog-mongo01:27017', 'graylog-mongo02:27017', 'graylog-mongo03:27017']  }  },
-  bind_ip => ['0.0.0.0']
-}->
-
-class {'::mongodb::client':}
+#class {'::mongodb::client':}
 
 }
