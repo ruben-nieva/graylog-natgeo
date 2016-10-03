3db231ba816517ef1fee6c827f0c3af8a83f12de
Ruben Nieva <ruben.nieva@globant.com>
2016-10-02 22:43:23 -0300

    modified mongo

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -3,11 +3,11 @@ $script_name = 'disable-transparent-hugepages'
 ){
 
 if $hostname == 'graylog-mongo02' {
-  notify ("Server is graylogmongo02")
+  #notify ("Server is graylogmongo02")
   $disable_hugepages = '/etc/init.d/disable_hugepages'
 }
 else{
-notify ("Server is $hostname")
+#notify ("Server is $hostname")
  $disable_hugepages = '/etc/init.d/disable-transparent-hugepages'
 }
 
