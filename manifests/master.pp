42f5ceb4b8c682faa023405e2027e691ed20fc00
Ruben Nieva <ruben.nieva@globant.com>
2016-10-03 16:32:00 -0300

    Updated graylog master server.

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,5 +1,9 @@
 class graylog_natgeo::master{
 
+$elastic_shards = hiera ('graylog::elasticsearch_shards')
+$elastic_replicas = hiera ('graylog::elasticsearch_replicas')
+$elastic_hosts = hiera_array ('graylog::elasticsearch_hosts')
+
 class { 'jdk_oracle': }
 
 class { 'graylog::repository':
@@ -18,13 +22,15 @@ class { 'graylog::server':
    'elasticsearch_cluster_name'                         => 'elasticsearch',
    'elasticsearch_network_host'                         => $hostname,
    'elasticsearch_discovery_zen_ping_multicast_enabled' => false,
-   'elasticsearch_discovery_zen_ping_unicast_hosts'     => "graylog-elasticsearch01:9300, graylog-elasticsearch02:9300, graylog-elasticsearch03:9300",
-   'elasticsearch_shards'                               => 4,
-   'elasticsearch_replicas'                             => 1,
+   #'elasticsearch_discovery_zen_ping_unicast_hosts'     => "graylog-elasticsearch01:9300, graylog-elasticsearch02:9300, graylog-elasticsearch03:9300",
+   'elasticsearch_discovery_zen_ping_unicast_hosts'     => $elastic_hosts,
+   'elasticsearch_shards'                               => $elastic_shards,
+   'elasticsearch_replicas'                             => $elastic_replicas,
    'rest_listen_uri'                                    => 'http://0.0.0.0:12900/',
    'rest_transport_uri'                                 => "http://$ipaddress_eth1:12900",
    'web_listen_uri'                                     => 'http://0.0.0.0:9000',
-   'mongodb_uri'                                        => 'mongodb://graylog-mongo01,graylog-mongo02,graylog-mongo03/graylog2',
+   #'mongodb_uri'                                        => 'mongodb://graylog-mongo01,graylog-mongo02,graylog-mongo03/graylog2',
+   'mongodb_uri'                                        => $mongodb_uri,
    'mongodb_max_connections'                            => 1000,
    'mongodb_threads_allowed_to_block_multiplier'        => 5,
   }
