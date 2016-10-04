708f1f160717062ca5c19f550c8c41b9eb6d1d02
Ruben Nieva <ruben.nieva@globant.com>
2016-10-04 09:59:00 -0300

    Added new hiera variable

--- manifests/elastic.pp
+++ manifests/elastic.pp
@@ -2,13 +2,14 @@ class graylog_natgeo::elastic{
 
  class { 'jdk_oracle': }
 
-$elastic_unicast_hosts = hiera_array('elastic_unicast_hosts')
+$elastic_unicast_hosts = hiera_array('ilm_elastic::elastic_hosts')
+$elastic_version = hiera('ilm_elastic::version')
 
  class { 'elasticsearch':
   ensure => 'present',
   manage_repo  => true,
   repo_version => '2.x',
-  version => '2.3.5',
+  version => $elastic_version,
   config => {
  	'cluster.name' => 'elasticsearch',
  	'discovery.zen.ping.unicast.enabled' => 'false',
