0ddcb21c471be21f111d7840fb7f70de3b424f07
Ruben Nieva <ruben.nieva@gmail.com>
2016-10-07 13:48:06 -0300

    Fixed array issue

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -9,13 +9,17 @@ class { 'filebeat':
 }
 
 filebeat::prospector { 'elastic-log':
-  paths    => '/var/log/elasticsearch/*/*.log*',
+  paths    => [
+    '/var/log/elasticsearch/*/*.log*',
+  ],
   doc_type => 'elastic-log',
   input_type => 'log',
 }
 
 filebeat::prospector { 'auth-log':
-  paths    => '/var/log/auth.log',
+  paths    => [
+    '/var/log/auth.log',
+  ],
   doc_type => 'auth-log',
 }
 
