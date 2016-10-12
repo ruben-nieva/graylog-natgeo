344f0a61c0f2fec3a47588fac0b2e08a2d948f3a
Ruben Nieva <ruben.nieva@globant.com>
2016-10-12 11:39:43 -0300

    get and config elastic cluster name from hiera

--- manifests/elastic.pp
+++ manifests/elastic.pp
@@ -17,15 +17,16 @@ class graylog_natgeo::elastic{
 $elastic_unicast_hosts = hiera_array("ilm_elastic::elastic_hosts::${::environment}")
 $elastic_version = hiera("ilm_elastic::version::${::environment}", '2.3.5')
 
+$elastic_cluster_name = "ilm-elastic-${::environment}"
+
  class { 'elasticsearch':
   ensure => 'present',
   manage_repo  => true,
   repo_version => '2.x',
   version => $elastic_version,
   config => {
- 	'cluster.name' => 'elasticsearch',
+ 	'cluster.name' => $elastic_cluster_name,
  	'discovery.zen.ping.unicast.enabled' => 'false',
- 	#'discovery.zen.ping.unicast.hosts'  => ['graylog-elasticsearch01', 'graylog-elasticsearch02', 'graylog-elasticsearch03'],
   'discovery.zen.ping.unicast.hosts'  => $elastic_unicast_hosts,
  	'network.host' => $hostname,
  	'http.port' => '9200',
