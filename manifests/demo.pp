1df47d8b2132a539fecd5cbb4392730fd9da6e50
reman68 <reman@stash.com>
2017-01-13 14:52:30 -0300

    New demo file

--- manifests/demo.pp
+++ manifests/demo.pp
@@ -7,6 +7,13 @@ file { "/var/tmp/demo.txt":
     mode   => '0644',
  }
 
+ file { "/var/tmp/demo2.txt":
+     ensure => present,
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+  }
+
  exec { "apt-get update":
     command => "/usr/bin/apt-get update",
     onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
